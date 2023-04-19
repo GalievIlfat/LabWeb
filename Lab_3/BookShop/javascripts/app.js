ab9c227184db080ef3d7e3f95f6ecb77497fc79d
Галиев И <ilfatgaliev452@gmail.com>
2023-04-19 12:15:24 +0300

    Преобразование кода с помощью функции

--- Lab_3/BookShop/javascripts/app.js
+++ Lab_3/BookShop/javascripts/app.js
@@ -1,5 +1,23 @@
 var main = function () {
     "use strict";
-    console.log("hello, world!");
+    var makeTabActive = function (tabNumber) {
+
+        var tabSelector = ".tabs a:nth-child(" + tabNumber + ") span";
+        $(".tabs span").removeClass("active");
+        $(tabSelector).addClass("active");
     };
-    $(document).ready(main)
+    $(".tabs a:nth-child(1)").on("click", function () { 
+        makeTabActive(1);
+        return false;
+    });
+    $(".tabs a:nth-child(2)").on("click", function () {
+        makeTabActive(2);
+        return false;
+    });
+    $(".tabs a:nth-child(3)").on("click", function () {
+        makeTabActive(3);
+        return false;
+    });
+};
+
+main();
